f586da08dd6de7b28054cd158ea4b798e5c2c8f7
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-20 16:04:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',()=>{
+	const message= document.createTextNode("DOM load success");
+	document.body.appendChild(message);
+})
